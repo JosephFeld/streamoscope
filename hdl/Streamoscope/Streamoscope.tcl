bf18e2bf0e0e5c360fba05a641e531e5a61424d4
Joseph Feld <josephfeld2019@gmail.com>
2024-08-23 14:02:25 -0400

    file paths are fixed for real (i think)

--- hdl/Streamoscope/Streamoscope.tcl
+++ hdl/Streamoscope/Streamoscope.tcl
@@ -81,7 +81,7 @@ proc checkRequiredFiles { origin_dir} {
   }
 
   set paths [list \
- "[file normalize "$origin_dir/../ADC-lvds-main/ip/ADC-lvds-main/ip"]"]"\
+ "[file normalize "$origin_dir/hdl/ADC-lvds-main/ip"]"]"\
   ]
   foreach ipath $paths {
     if { ![file isdirectory $ipath] } {
@@ -259,10 +259,10 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
   create_fileset -srcset sources_1
 }
 
-# Set IP repository paths
+# Set IP repository paths 
 set obj [get_filesets sources_1]
 if { $obj != {} } {
-   set_property "ip_repo_paths" "[file normalize "$origin_dir/../ADC-lvds-main/ip"]" $obj
+   set_property "ip_repo_paths" "[file normalize "$origin_dir/hdl/ADC-lvds-main/ip"]" $obj
 
    # Rebuild user ip_repo's index before adding any source files
    update_ip_catalog -rebuild
