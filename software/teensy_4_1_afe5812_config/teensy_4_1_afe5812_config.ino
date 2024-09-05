7402ce2e6db21cddeebdd7de6e6c0865cd136cad
Joseph Feld <josephfeld2019@gmail.com>
2024-09-05 10:17:49 -0400

    cleaned up teensy

--- software/teensy_4_1_afe5812_config/teensy_4_1_afe5812_config.ino
+++ software/teensy_4_1_afe5812_config/teensy_4_1_afe5812_config.ino
@@ -1,17 +1,5 @@
 #include <SPI.h>
 
-// set pin 10 as the slave select for the digital pot:
-const int slaveSelectPin = 10;
-
-//CDC configuration
-//MSB to LSB
-//see page 5 https://www.ti.com/lit/ds/symlink/cdc7005.pdf?ts=1717427064621
-const int word0[4] = { 0b01101010, 0b10100100, 0b00000001, 0b11111100 };
-const int word1[4] = { 0b01011011, 0b01000100, 0b00000001, 0b11111101 };
-const int word2[4] = { 0b00000000, 0b00000000, 0b00000000, 0b00111010 };
-const int word3[4] = { 0b00000000, 0b00000000, 0b00000000, 0b00000011 };
-const int* words[4] = { word0, word1, word2, word3 };
-
 const uint8_t AFE_RESET = 3;
 const uint8_t AFE_PDN_VCA = 4;
 const uint8_t AFE_PDN_ADC = 5;
@@ -76,11 +64,7 @@ void spi_write(uint8_t data_pin, uint8_t clock_pin, uint32_t freq, uint8_t bit_o
 
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
-
-  pinMode(slaveSelectPin, OUTPUT);
-  digitalWrite(slaveSelectPin, HIGH);
 
   pinMode(AFE_MISO_PIN, INPUT);
   pinMode(AFE_MOSI_PIN, OUTPUT);
@@ -93,8 +77,6 @@ void setup() {
   pinMode(AFE_PDN_GLOBAL, OUTPUT);
 
 
-  // initialize SPI:
-  // SPI1.begin();
   digitalWrite(AFE_PDN_VCA, 0);
   digitalWrite(AFE_PDN_ADC, 0);
   digitalWrite(AFE_PDN_GLOBAL, 0);
@@ -108,12 +90,10 @@ void setup() {
   
   delay(10);
 
-  // configureCDC7005();
   loopbackAFE();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   // digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   // delay(500);                       // wait for a second
   // digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
@@ -122,67 +102,43 @@ void loop() {
   // loopbackAFE();
 }
 
-/*
-{OUT2B_P,OUT1B_P,OUT1A_P,OUT2A_P}
-afe   3         1        4     2
-fpga  3         2         1    0 
-
-=--------------
-FR      4
-2       1 
-3    
-   
-        DCO
---------------
-
-=--------------
-FR      1A
-2A      1B
-2B    
-   
-        DCO
---------------
-  
-*/
-
-
-
 void loopbackAFE() {
 
   int freq = 10000;  //hz
 
+  // Ping AFE to send data over MISO
+  if(false) {
+    //write BEEF to a register
+    digitalWrite(AFE_CE_PIN, 0);
+    spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x031100);
+    digitalWrite(AFE_CE_PIN, 1);
 
-  //write BEEF to a register
-  // digitalWrite(AFE_CE_PIN, 0);
-  // spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x031100);
-  // digitalWrite(AFE_CE_PIN, 1);
-
-  // delay(5);
-
-
-  // //write to enable reads
-  // digitalWrite(AFE_CE_PIN, 0);
-  // spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x000002);
-  // digitalWrite(AFE_CE_PIN, 1);
-
-  // delay(5);
+    delay(5);
 
 
-  // //read BEEF from the register
-  // digitalWrite(AFE_CE_PIN, 0);
-  // spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x031100);
-  // digitalWrite(AFE_CE_PIN, 1);
-  
+    // //write to enable reads
+    digitalWrite(AFE_CE_PIN, 0);
+    spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x000002);
+    digitalWrite(AFE_CE_PIN, 1);
+
+    delay(5);
 
 
-  // delay(5);
+    // //read BEEF from the register
+    digitalWrite(AFE_CE_PIN, 0);
+    spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x031100);
+    digitalWrite(AFE_CE_PIN, 1);
+    
 
 
-  // //write to enable writes
-  // digitalWrite(AFE_CE_PIN, 0);
-  // spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x000000);
-  // digitalWrite(AFE_CE_PIN, 1);
+    delay(5);
 
+
+    // //write to enable writes
+    digitalWrite(AFE_CE_PIN, 0);
+    spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x000000);
+    digitalWrite(AFE_CE_PIN, 1);
+  }
   //real config
 
   digitalWrite(AFE_CE_PIN, 0);
@@ -198,7 +154,7 @@ void loopbackAFE() {
 
   delay(5);
 
-  int channel_enable = 0b00000101; //1=on, 0=off
+  int channel_enable = 0b00000101; //1=on, 0=off. Zero on the right
 
   // LNA Gain
   // 2 bits 
@@ -241,50 +197,19 @@ void loopbackAFE() {
   int test_pattern = 0b000;
 
   digitalWrite(AFE_CE_PIN, 0);
-  spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x0207F8 ^ (channel_enable << 3) | (test_pattern << 13));//E=ramp, 8=1s, LVDS shut down. Channel 1 analog 0x0207F0
-                                                                                // Channel 8 Ramp 0x02E3F8
-  digitalWrite(AFE_CE_PIN, 1);
-
-  //LVDS shut down 10:3
-  /*
-  7F8 all channels
-
-  1 2 3 4
-  0 0 0 0   0x02E7F8
-  1 0 0 0   0x02E7F0
-  0 1 0 0   0x02E7E8
-  1 1 0 0   0x02E7E0
-  0 0 1 0   0x02E7D8
-  1 0 1 0   0x02E7D0
-  0 1 1 0   0x02E7C8
-  1 1 1 0   0x02E7C0
-  0 0 0 1   0x02E7B8
-  1 0 0 1   0x02E7B0
-  0 1 0 1   0x02E7A8
-  1 1 0 1   0x02E7A0
-  0 0 1 1   0x02E798
-  1 0 1 1   0x02E790
-  0 1 1 1   0x02E788
-  1 1 1 1   0x02E780
-
-
-  */
-  delay(5);
-
-  digitalWrite(AFE_CE_PIN, 0);
-  spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x0183FC ^ (channel_enable << 2)); //shut down ADCs. Channel 1 on: 0x0183F8. 1&3: 0x0183E8
-                                                                                 //power down is bits [9:2]
-                                                                                 //1&2 0x0183F0
-                                                                                 //8 0x0181FC
+  spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x0207F8 ^ (channel_enable << 3) | (test_pattern << 13));
   digitalWrite(AFE_CE_PIN, 1);
 
   delay(5);
 
   digitalWrite(AFE_CE_PIN, 0);
-  spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x3500FF ^ (channel_enable << 0)); //shut down LNAs. Channel 1 on: 0x3500FE 1&3: 0x3500FA
-                                                                                 //power down is bits [7:0]
-                                                                                 //1&2 0x3500FC
-                                                                                 //8 0x35007F
+  spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x0183FC ^ (channel_enable << 2)); //shut down ADCs
+  digitalWrite(AFE_CE_PIN, 1);
+
+  delay(5);
+
+  digitalWrite(AFE_CE_PIN, 0);
+  spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x3500FF ^ (channel_enable << 0)); //shut down LNAs
   digitalWrite(AFE_CE_PIN, 1);
 
   delay(5);
@@ -296,6 +221,7 @@ void loopbackAFE() {
   delay(5);
 
   // AFE5812 only
+  // If this is run on the AFE5808A, it shuts the chip down
   digitalWrite(AFE_CE_PIN, 0);
   spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x160001); //turn off demodulator
   digitalWrite(AFE_CE_PIN, 1);
@@ -303,43 +229,22 @@ void loopbackAFE() {
   delay(5);
 
   digitalWrite(AFE_CE_PIN, 0);
-  spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x340000 | (act_term << 8) | (lna_gain << 13)); //active termination 0x342100 vs not 0x342000
+  spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x340000 | (act_term << 8) | (lna_gain << 13));
   digitalWrite(AFE_CE_PIN, 1);
 
   delay(5);
 
   digitalWrite(AFE_CE_PIN, 0);
-  spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x330008 | (pga_gain << 13)); //PGA gain 30 db: 0x332008, 24db: 0x330008
+  spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x330008 | (pga_gain << 13));
   digitalWrite(AFE_CE_PIN, 1);
 
   delay(5);
 
   digitalWrite(AFE_CE_PIN, 0);
-  spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x3B0088 | (vca_attn << 4)); //Attenuation, 0 db 0x3B0088, -30 db 0x3B00D8
+  spi_write(AFE_MOSI_PIN, AFE_SCK_PIN, freq, MSBFIRST, SPI_MODE3, 24, 0x3B0088 | (vca_attn << 4));
   digitalWrite(AFE_CE_PIN, 1);
 
   delay(5);
 
 
-}
-
-//address goes from 0-3
-void configureCDC7005() {
-
-  // write each data word
-  for (int i = 0; i < 4; i++) {
-    // gain control of the SPI port
-    // and configure settings
-    SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE0));
-    // take the SS pin low to select the chip:
-    digitalWrite(slaveSelectPin, LOW);
-    //write each byte in the word
-    for (int j = 3; j >= 0; j--) {
-      SPI.transfer(words[i][j]);
-    }
-    // take the SS pin high to de-select the chip:
-    digitalWrite(slaveSelectPin, HIGH);
-    // release control of the SPI port
-    SPI.endTransaction();
-  }
 }
